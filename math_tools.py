9957b1b147139cdb4b40ce0aa58f2d2ff72bc90a
mavrik2 <mavrik2103@gmail.com>
2026-02-12 10:44:00 +0300

    update

---
+++ math_tools.py
@@ -0,0 +1,4 @@
+def square(number):
+    return number ** 2
+def is_even(number):
+    return number % 2 == 0
---
+++ my_module.py
@@ -0,0 +1,4 @@
+def greet(name):
+    return "Привет, " + name + "!"
+def add_numbers(a, b):
+    return a + b
